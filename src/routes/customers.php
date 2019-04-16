7a8a8eb7590e9e4cf0351363cb3d6810e9777a19
Temitope Ayotunde <brhamix@gmail.com>
2019-04-16 11:08:45 +0100

    Initial Commit

---
+++ src/routes/customers.php
@@ -0,0 +1,10 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+// Get all customers
+$app->get('/api/customers', function(Request $request, Response $reponse){
+    
+});
